5e1543bf4f6283f25ef4296b49cb352b348dddd5
akashpande1886 <72142288+akashpande1886@users.noreply.github.com>
2025-04-21 20:04:02 +0530

    ngfor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Room } from './rooms';
-import { NgIf } from '@angular/common';
+import { Room, RoomList } from './rooms';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-rooms',
-  imports: [NgIf],
+  imports: [NgIf, NgFor],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss',
 })
@@ -20,5 +20,25 @@ export class RoomsComponent implements OnInit {
     availableRooms: 10,
     bookedRooms: 5,
   };
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'AC, Free wifi, Bathroom,Kitchen',
+      price: 400,
+      photos: 'https://unsplash.com/s/photos/living-room',
+      checkinTime: new Date('11-Nov,2021'),
+      checkoutTime: new Date('12-Nov,2021'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'AC, Free wifi, TV, Bathroom,Kitchen',
+      price: 500,
+      photos: 'https://unsplash.com/s/photos/living-room',
+      checkinTime: new Date('11-Nov,2021'),
+      checkoutTime: new Date('12-Nov,2021'),
+    },
+  ];
   ngOnInit(): void {}
 }
